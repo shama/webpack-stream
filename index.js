93edcb361201ed048d5382610f9b7a2af8c484dc
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-09 13:47:50 +0100

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+'use strict';
+module.exports = function () {
+	throw 'gulp-webpack: You don\'t need a gulp plugin for this. Just use webpack directly.';
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+# [gulp](https://github.com/wearefractal/gulp)-webpack
+
+> No need for a gulp plugin. Just use webpack directly.
